958f5e9e934e327f6e743903cdc8a57a115de619
aakintil <aakintil@andrew.cmu.edu>
2013-11-21 23:18:48 -0500

    getting somewhere

--- js/application.js
+++ js/application.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  
+
   // vertically centering the home navigation elements
   var work = $("#work"), 
   resume = $("#resume"), 
@@ -12,6 +12,7 @@ $(document).ready(function() {
   }
 
   centerElements($(".nav div")); 
+  centerElements($(".circle")); 
 
 
   $(".nav").hover(function(){
@@ -22,20 +23,33 @@ $(document).ready(function() {
   },function() {
     $(this).animate({ "background-color" : "transparent", opacity : 0.2 }, 1000);
   });
-  
+
   // animating the click
   $(".nav").on("click", function() {
     $("#home").animate({ height: 0 }, 1000, function() {  $(this).hide(); $("#main").show("slow") }); 
   }); 
 
+
+  $("#first").on("click", function() {
+    $("#content").hide("slow"); 
+    console.log(pages.project1)
+    $("#title").html(pages.project1.title); 
+    $("#header").css({ backgroundImg : pages.project1.img_src})
+  })
+
+  // end of on load
 }); 
 
 
 function centerElements(el) {
   var parent = $(el).parent(); 
 
-  $(el).css(
-    {
-      marginTop: (parent.height() - $(el).outerHeight()) / 2
-    });
+  $(el).css({ marginTop: (parent.height() - $(el).outerHeight()) / 2 });
+}
+
+var pages = {
+  project1 : {
+    title : "PPTV", 
+    img_src : "images/bg.gif"
   }
+}
