5ed207bd226d39cf1ead7c9fc4d687d4fb544980
aakintil <aakintil@andrew.cmu.edu>
2013-11-25 16:22:56 -0500

    figuring out the layout for project page

--- js/application.js
+++ js/application.js
@@ -31,10 +31,12 @@ $(document).ready(function() {
 
 
   $("#first").on("click", function() {
-    $("#content").hide("slow"); 
+    $("#content").children().hide("slow"); 
     console.log(pages.project1)
     $("#title").html(pages.project1.title); 
-    $("#header").css({ backgroundImg : pages.project1.img_src})
+    $("#header").css({ "background-image" : "url("+pages.project1.img_src+")"}); 
+    $("#content").append(pages.project1.content.synopsis); 
+
   })
 
   // end of on load
@@ -47,9 +49,27 @@ function centerElements(el) {
   $(el).css({ marginTop: (parent.height() - $(el).outerHeight()) / 2 });
 }
 
+// might make a pages js file later
 var pages = {
   project1 : {
     title : "PPTV", 
-    img_src : "images/bg.gif"
+    img_src : "images/bg.gif", 
+    images : [""], 
+    content : {
+      synopsis : "This is a project I worked on during parts of my junior and senior years under Scott Davidoff and John Zimmerman. The main design for PPTV had already been produced, so I was tasked with the interaction design and information architecture of system. I created a prototype for manual data entry that looked into the information architecture of the system and a prototype for exhibiting the interaction that would occur if a user were to use the system's menu. Full documentation for the interaction design can be viewed here. Full documentation for the information architecture can be viewed here", 
+      skills : "Adobe InDesign CS6, Adobe Illustrator CS6, Adobe Photoshop CS6, paper prototypes, Balsamiq, HTML, CSS",  
+    }
+  }, 
+
+  project2 : {
+    title : "SnakeValance", 
+    img_src : ""
+  }, 
+
+  project3 : {
+    title : "Tea", 
+    img_src : ""
   }
+
+
 }
