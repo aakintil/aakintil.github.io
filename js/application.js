c1d3d31754db693a7d843fea06b96654e81428d8
aakintil <aakintil@andrew.cmu.edu>
2013-11-27 02:46:21 -0500

    making progress

--- js/application.js
+++ js/application.js
@@ -36,6 +36,8 @@ $(document).ready(function() {
     $("#title").html(pages.project1.title); 
     $("#header").css({ "background-image" : "url("+pages.project1.img_src+")"}); 
     $("#content").append(pages.project1.content.synopsis); 
+    $("#content").append(pages.project1.content.skills); 
+    $("#content").append(pages.project1.images); 
 
   })
 
@@ -54,10 +56,10 @@ var pages = {
   project1 : {
     title : "PPTV", 
     img_src : "images/bg.gif", 
-    images : [""], 
+    images : "<div class='col-md-3 col-md-offset-3'> d</div> <div class='col-md-3'> d</div> <div class='col-md-4 col-md-offset-2'> d</div> <div class='col-md-4'> d</div>", 
     content : {
-      synopsis : "This is a project I worked on during parts of my junior and senior years under Scott Davidoff and John Zimmerman. The main design for PPTV had already been produced, so I was tasked with the interaction design and information architecture of system. I created a prototype for manual data entry that looked into the information architecture of the system and a prototype for exhibiting the interaction that would occur if a user were to use the system's menu. Full documentation for the interaction design can be viewed here. Full documentation for the information architecture can be viewed here", 
-      skills : "Adobe InDesign CS6, Adobe Illustrator CS6, Adobe Photoshop CS6, paper prototypes, Balsamiq, HTML, CSS",  
+      synopsis : "<p id='blurb' class='col-md-12'>This is a project I worked on during parts of my junior and senior years under Scott Davidoff and John Zimmerman. The main design for PPTV had already been produced, so I was tasked with the interaction design and information architecture of system. I created a prototype for manual data entry that looked into the information architecture of the system and a prototype for exhibiting the interaction that would occur if a user were to use the system's menu. Full documentation for the interaction design can be viewed here. Full documentation for the information architecture can be viewed here</p>", 
+      skills : "<div id='skills' class='col-md-12'> Adobe InDesign CS6, Adobe Illustrator CS6, Adobe Photoshop CS6, paper prototypes, Balsamiq, HTML, CSS</div>",  
     }
   }, 
 
