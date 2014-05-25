2d9dadf82e2b79bd2ffc5ab162555eb90a1b1812
aakintil <aakintil@andrew.cmu.edu>
2014-05-25 16:39:27 -0700

    leaving deni and aneekas

--- js/application.js
+++ js/application.js
@@ -4,9 +4,12 @@
 
 // Document ready 
 $(document).ready( function() {
-$("#nav ul li").hover( function() {
-  // alert("hover"); 
-})
+  $("#nav ul li").hover( function() {
+    var el = $(this).attr("class"); 
+    
+    var highlight = { work: "got it", contact: "", about: ""}
+    console.log( highlight[ el ] );  
+  })
 
 
 }); 
