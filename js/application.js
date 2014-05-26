d9f1b159beaf4bcc9241ea419e6281841a8c7ed3
aakintil <aakintil@andrew.cmu.edu>
2014-05-25 21:10:08 -0700

    getting the highlight colors for the navigation links

--- js/application.js
+++ js/application.js
@@ -7,7 +7,7 @@ $(document).ready( function() {
   $("#nav ul li").hover( function() {
     var el = $(this).attr("class"); 
     
-    var highlight = { work: "got it", contact: "", about: ""}
+    var highlight = { work: "", contact: "", about: ""}
     console.log( highlight[ el ] );  
   })
 
