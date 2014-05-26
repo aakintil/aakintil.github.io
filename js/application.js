3d9683a2c89da74611fd69ce44222a94cb371e71
aakintil <aakintil@andrew.cmu.edu>
2014-05-25 21:28:15 -0700

    looking good working on the hover effect for nav

--- js/application.js
+++ js/application.js
@@ -2,13 +2,22 @@
 // Main Application File
 /////////////////////////
 
+var highlight = { 
+  work: "#4ad6de", 
+  contact: "#428cff", 
+  about: "#613db5"
+}
+
 // Document ready 
 $(document).ready( function() {
   $("#nav ul li").hover( function() {
     var el = $(this).attr("class"); 
-    
-    var highlight = { work: "", contact: "", about: ""}
-    console.log( highlight[ el ] );  
+    var border_color = "2px solid " + highlight[ el ];
+    $( this ).css({ border: border_color })  
+  }, 
+  function() {
+    if ( !$( this ).hasClass("active") )
+    $( this ).css({ border: "2px solid transparent" }); 
   })
 
 
