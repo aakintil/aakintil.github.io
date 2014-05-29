cfb53304d623e891602eb3ad8f9c48e819e550e5
aakintil <aakintil@andrew.cmu.edu>
2014-05-28 18:03:25 -0700

    added slight opacity animation...keep it simple...is it enough?

--- js/application.js
+++ js/application.js
@@ -10,6 +10,15 @@ var highlight = {
 
 // Document ready 
 $(document).ready( function() {
+  // slowly load and animate the page
+  $("#nav").animate({ opacity: 1 }, 1900, function() {
+    $("#content").animate({ opacity: 1 }, 1500, function() {
+      $("#footer").animate({ opacity: 1 }, 1000)
+    })
+  }); 
+  
+  
+  // different color hover effects for menu links
   $("#nav ul li").hover( function() {
     var el = $(this).attr("class"); 
     var border_color = "2px solid " + highlight[ el ];
@@ -19,6 +28,7 @@ $(document).ready( function() {
     if ( !$( this ).hasClass("active") )
     $( this ).css({ border: "2px solid transparent" }); 
   })
+
 
 
 }); 
