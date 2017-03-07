ac99f5e6e337acae7be1cb429b9b8d115799882c
Aderinsola Akintilo <aderinsola419@gmail.com>
2017-03-07 00:27:51 -0800

    merging with masteR

--- js/app.js
+++ js/app.js
@@ -1,36 +1,14 @@
 $(document).ready(function () {
-     console.log("ready");
-     var untilDate = new Date("2017/02/15");
-     console.log(untilDate.toDateString())
-     $('body').countdown("2017/03/28", function (event) {
-          $(".days").html(event.strftime('%D'));
-          $(".hrs").text(event.strftime('%H'));
-          $(".mins").text(event.strftime('%M'));
-          $(".secs").text(event.strftime('%S'));
-          // event.strftime('%W weeks %-d days %-H h %M min %S sec');
-     });
-     var overlay = $("#animation")
-          , content = $(".container-fluid .row");
 
-     function hideOverlay() {
-          setTimeout(function () {
-               overlay.addClass("animated fadeOut");
-               animateText();
-          }, 350)
-     };
+     var content = $(".container-fluid .row"),
+          img = $("img");
 
      function animateText() {
           content.addClass("fadeInUp");
-          setTimeout(function () {
-               modifyOverlayCSS();
-          }, 750);
+          img.addClass("fadeInUp");
      }
 
-     function modifyOverlayCSS() {
-          overlay.css({
-               "z-index": 0
-               , "display": "none"
-          })
-     }
-     hideOverlay();
+     setTimeout(function () {
+          animateText();
+     }, 750)
 })
