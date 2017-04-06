d58ef9d5dea9631fa11f278df10d1b96ee8e12be
Aderinsola Akintilo <aderinsola419@gmail.com>
2017-04-05 23:43:18 -0700

    fixed skeleton layout for site

--- build/js/templates.js
+++ build/js/templates.js
@@ -2,7 +2,7 @@ this.JST = {"views/main/main": function(obj) {
 obj || (obj = {});
 var __t, __p = '', __e = _.escape;
 with (obj) {
-__p += '<!-- Header -->\n<div class="layout--header row marg--none">\n\t<div class="col-sm-12">\n\t\tHeader\n\t</div>\n</div>\n<!-- / Header -->\n\n<!-- Main Content -->\n<div class="layout--content row marg--none">\n\n\t<div class="content--top col-sm-12">\n\t\tcontent top\n\t</div>\n\n\t<div class="content--bottom col-sm-12">\n\t\tcontent bottom\n\t</div>\n</div>\n<!-- / Main Content -->';
+__p += '<!-- Header -->\n<div class="layout--header row marg--none">\n\t<div class="col-sm-12 pad--none">\n\t\tHeader\n\t</div>\n</div>\n<!-- / Header -->\n\n<!-- Main Content -->\n<div class="layout--content row marg--none">\n\n\t<div class="content--top col-sm-12 pad--none">\n\t\tcontent top\n\t</div>\n\n\t<div class="content--bottom col-sm-12 pad--none">\n\t\tcontent bottom\n\t</div>\n</div>\n<!-- / Main Content -->';
 
 }
 return __p
